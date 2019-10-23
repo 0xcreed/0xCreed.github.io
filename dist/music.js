6363793509535f1094ea4a9986aed1670f4e6d3f
AssCreed <792683096@qq.com>
2019-10-23 16:36:11 +0800

    Site updated: 2019-10-23 16:36:06

--- dist/music.js
+++ dist/music.js
@@ -1,7 +1,7 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,   //吸底模式
-    autoplay: true, //自动播放
+    autoplay: false, //自动播放
     theme: '#FADFA3', //主题色
     loop: 'all', //音频循环播放, 可选值: 'all'全部循环, 'one'单曲循环, 'none'不循环
     order: 'random', //音频循环顺序, 可选值: 'list'列表循环, 'random'随机循环
